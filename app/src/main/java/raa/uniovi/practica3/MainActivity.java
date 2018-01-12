251520666ef351f2e48567c578170550409a49a2
SergioFair <sergiomd15@hotmail.com>
2018-01-12 12:55:26 +0100

    Arreglada ruta

--- app/src/main/java/raa/uniovi/practica3/MainActivity.java
+++ app/src/main/java/raa/uniovi/practica3/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
             architectView.onPostCreate();
         }
         try{
-            this.architectView.load("practica3/index.html");
+            this.architectView.load("samples/practica3/index.html");
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE
                             , Manifest.permission.ACCESS_COARSE_LOCATION
