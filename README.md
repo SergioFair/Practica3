3885ef939d2bfc04261e5f2347ea52fd23be0d98
SergioFair <sergiomd15@hotmail.com>
2018-01-12 10:52:55 +0100

    Proyecto inicial

--- README.md
+++
@@ -1,5 +0,0 @@
-# Práctica obligatoria de Realidad Aumentada
-El trabajo consistirá en desarrollar una aplicación de realidad aumentada para facilitar la accesibilidad de los niños a la hora de aprender inglés. Se mostrarán distintas imágenes que interactuarán con el usuario. Una vez sean pulsadas, se utilizará la utilidad Text To Speech de Google para decir su nombre en inglés.
-## Características:
-- Versión Android 4.4 KitKat.
-- Wikitude SDK 7.1.0
--- app/src/main/java/raa/uniovi/practica3/MainActivity.java
+++ app/src/main/java/raa/uniovi/practica3/MainActivity.java
@@ -1,63 +1,85 @@
 package raa.uniovi.practica3;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.speech.tts.TextToSpeech;
+import android.Manifest;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 
-import java.util.Locale;
+import com.wikitude.architect.ArchitectStartupConfiguration;
+import com.wikitude.architect.ArchitectView;
 
-public class MainActivity extends Activity {
+import java.io.IOException;
 
-    String text;
-    TextToSpeech tts;
+public class MainActivity extends AppCompatActivity {
+
+    private String key = "KwWUaRJOus03Oubdus7Wrrh4C9l+sHkfRiiKG0KJ8VSSkyFmwthHeQVvvk4Rv99fgxTZMutCpnhj3q2fb2STJsWBPO7GzBL4tylA61QHTQV6ewUO5QumW5rLmEhsW6EsFT5so4at+xw65RJGsyAzsuTwKRoRvmU++tsyNYvb7mFTYWx0ZWRfXyxsk+l6cdtFIZkdfQwgBmN2dAVarIIEZgvYoW1+A9UeUgDN317jgZVwx1VPuFLsc5C/fBFRuJNiYt/bzoFvVbvQKb3e1LuxinA63/JsXLQoolQYpHGy6q0hVo8Aa7TAioxS3rO8ZnS1JDqIHwNsxT8HoDCGDZS8TK813P8zSFhZa82NMVghg8kT66TaOzAD9wKrIi7WItm6Ht8wu4BkmbbKaX7GKlt1EK4FCzfDg+5oa/BSwNpd/jGHR/Bzeq35kN2n+EUSDihxD0ho3OqPzIJtsIOKa1kV7rO6fDNII3foP0yxo5NkhdRbMoPCgBbe9nub+uxA52Iov06dKtNnDx8UXGhuLugKyGC5DeEOLXlO4RvZ/n41b8Lxb91OnNvtyXYIaPZHbQLNPoQnabRgQ4tWO2fuLczxwaw3cSflYB9ZQMoeKTlroF4OKi+UeMfzCP7iyF1s92w7Wq3dzezDYA1AczIyekrm1mXLAjJdOOE4hzYlLHNTn20=";
+    protected ArchitectView architectView;
+    protected ArchitectStartupConfiguration config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tts=new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if(status == TextToSpeech.SUCCESS){
-                    int result=tts.setLanguage(Locale.US);
-                    if(result==TextToSpeech.LANG_MISSING_DATA ||
-                            result==TextToSpeech.LANG_NOT_SUPPORTED){
-                        Log.e("error", "This Language is not supported");
-                    }
-                }
-                else
-                    Log.e("error", "Initilization Failed!");
-            }
-        });
+        architectView = findViewById(R.id.architectView);
+        config = new ArchitectStartupConfiguration();
+        config.setLicenseKey(key);
+        this.architectView.onCreate(config);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        if(this.architectView != null){
+            architectView.onPostCreate();
+        }
+        try{
+            this.architectView.load("samples/01_ImageTracking_1_ImageOnTarget/index.html");
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE
+                            , Manifest.permission.ACCESS_COARSE_LOCATION
+                            , Manifest.permission.ACCESS_FINE_LOCATION
+                            , Manifest.permission.ACCESS_NETWORK_STATE
+                            , Manifest.permission.ACCESS_WIFI_STATE
+                            , Manifest.permission.BLUETOOTH
+                            , Manifest.permission.CAMERA
+                            , Manifest.permission.INTERNET},
+                    1);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        super.onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        if(this.architectView != null){
+            this.architectView.onLowMemory();
+        }
     }
 
     @Override
     protected void onPause() {
-        if(tts != null){
-            tts.stop();
-            tts.shutdown();
-        }
         super.onPause();
-    }
-
-    public void speech(View v){
-        ConvertTextToSpeech();
-    }
-
-    private void ConvertTextToSpeech() {
-        text = getString(R.string.prueba_speech);
-        if("".equals(text)){
-            text = "Content not available";
+        if(this.architectView != null){
+            this.architectView.onPause();
         }
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 
-    public void launchExperience(View v){
-        Intent intent = new Intent(this, WikitudeActivity.class);
-        startActivity(intent);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(this.architectView != null){
+            this.architectView.onDestroy();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(this.architectView != null){
+            this.architectView.onResume();
+        }
     }
 }
